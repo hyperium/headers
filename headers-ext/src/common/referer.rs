932dbd29ba07c9e6694e67b6d579629f9cdde48e
Sean McArthur <sean@seanmonstar.com>
2018-09-05 14:09:40 -0700

    re-enable Referer

--- headers-ext/src/common/referer.rs
+++ headers-ext/src/common/referer.rs
@@ -1,49 +1,41 @@
-#[derive(Debug, Clone, PartialEq, Header)]
-pub struct Referer(String);
-/*
-header! {
-    /// `Referer` header, defined in
-    /// [RFC7231](http://tools.ietf.org/html/rfc7231#section-5.5.2)
-    ///
-    /// The `Referer` [sic] header field allows the user agent to specify a
-    /// URI reference for the resource from which the target URI was obtained
-    /// (i.e., the "referrer", though the field name is misspelled).  A user
-    /// agent MUST NOT include the fragment and userinfo components of the
-    /// URI reference, if any, when generating the Referer field value.
-    ///
-    /// # ABNF
-    ///
-    /// ```text
-    /// Referer = absolute-URI / partial-URI
-    /// ```
-    ///
-    /// # Example values
-    ///
-    /// * `http://www.example.org/hypertext/Overview.html`
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use headers::{Headers, Referer};
-    ///
-    /// let mut headers = Headers::new();
-    /// headers.set(Referer::new("/People.html#tim"));
-    /// ```
-    ///
-    /// ```
-    /// use headers::{Headers, Referer};
-    ///
-    /// let mut headers = Headers::new();
-    /// headers.set(Referer::new("http://www.example.com/index.html"));
-    /// ```
-    // TODO Use URL
-    (Referer, REFERER) => Cow[str]
+use http::header::HeaderValue;
 
-    test_referer {
-        // Testcase from the RFC
-        test_header!(test1, vec![b"http://www.example.org/hypertext/Overview.html"]);
+/// `Referer` header, defined in
+/// [RFC7231](http://tools.ietf.org/html/rfc7231#section-5.5.2)
+///
+/// The `Referer` [sic] header field allows the user agent to specify a
+/// URI reference for the resource from which the target URI was obtained
+/// (i.e., the "referrer", though the field name is misspelled).  A user
+/// agent MUST NOT include the fragment and userinfo components of the
+/// URI reference, if any, when generating the Referer field value.
+///
+/// ## ABNF
+///
+/// ```text
+/// Referer = absolute-URI / partial-URI
+/// ```
+///
+/// ## Example values
+///
+/// * `http://www.example.org/hypertext/Overview.html`
+///
+/// # Examples
+///
+/// ```
+/// use headers::{Referer};
+///
+/// let r = Referer::from_static("/People.html#tim");
+/// ```
+#[derive(Debug, Clone, PartialEq, Header)]
+pub struct Referer(HeaderValue);
+
+impl Referer {
+    /// Create a `Referer` with a static string.
+    ///
+    /// # Panic
+    ///
+    /// Panics if the string is not a legal header value.
+    pub fn from_static(s: &'static str) -> Referer {
+        Referer(HeaderValue::from_static(s))
     }
 }
-
-bench_header!(bench, Referer, { vec![b"http://foo.com/hello:3000".to_vec()] });
-*/
