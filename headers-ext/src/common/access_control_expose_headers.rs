8e8c43d72822786ef8a04dc6c4dc093ea04e305d
Sean McArthur <sean@seanmonstar.com>
2018-09-12 17:02:18 -0700

    re-enable AccessControlExposeHeaders

--- headers-ext/src/common/access_control_expose_headers.rs
+++ headers-ext/src/common/access_control_expose_headers.rs
@@ -1,63 +1,47 @@
-use unicase::Ascii;
+/// `Access-Control-Expose-Headers` header, part of
+/// [CORS](http://www.w3.org/TR/cors/#access-control-expose-headers-response-header)
+///
+/// The Access-Control-Expose-Headers header indicates which headers are safe to expose to the
+/// API of a CORS API specification.
+///
+/// # ABNF
+///
+/// ```text
+/// Access-Control-Expose-Headers = "Access-Control-Expose-Headers" ":" #field-name
+/// ```
+///
+/// # Example values
+/// * `ETag, Content-Length`
+///
+/// # Examples
+///
+/// ```
+/// # extern crate headers_ext as headers;
+/// extern crate http;
+/// # fn main() {
+/// use http::header::{CONTENT_LENGTH, E_TAG};
+/// use headers::AccessControlExposeHeaders;
+///
+/// let expose = AccessControlExposeHeaders::new([
+///     CONTENT_LENGTH,
+///     E_TAG,
+/// ]);
+/// # }
+/// ```
+#[derive(Clone, Debug, Header)]
+#[header(csv)]
+pub struct AccessControlExposeHeaders(Vec<::HeaderName>);
 
-header! {
-    /// `Access-Control-Expose-Headers` header, part of
-    /// [CORS](http://www.w3.org/TR/cors/#access-control-expose-headers-response-header)
-    ///
-    /// The Access-Control-Expose-Headers header indicates which headers are safe to expose to the
-    /// API of a CORS API specification.
-    ///
-    /// # ABNF
-    ///
-    /// ```text
-    /// Access-Control-Expose-Headers = "Access-Control-Expose-Headers" ":" #field-name
-    /// ```
-    ///
-    /// # Example values
-    /// * `ETag, Content-Length`
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # extern crate headers;
-    /// # extern crate unicase;
-    /// # fn main() {
-    /// // extern crate unicase;
-    ///
-    /// use headers::{Headers, AccessControlExposeHeaders};
-    /// use unicase::Ascii;
-    ///
-    /// let mut headers = Headers::new();
-    /// headers.set(
-    ///     AccessControlExposeHeaders(vec![
-    ///         Ascii::new("etag".to_owned()),
-    ///         Ascii::new("content-length".to_owned())
-    ///     ])
-    /// );
-    /// # }
-    /// ```
-    ///
-    /// ```
-    /// # extern crate headers;
-    /// # extern crate unicase;
-    /// # fn main() {
-    /// // extern crate unicase;
-    ///
-    /// use headers::{Headers, AccessControlExposeHeaders};
-    /// use unicase::Ascii;
-    ///
-    /// let mut headers = Headers::new();
-    /// headers.set(
-    ///     AccessControlExposeHeaders(vec![
-    ///         Ascii::new("etag".to_owned()),
-    ///         Ascii::new("content-length".to_owned())
-    ///     ])
-    /// );
-    /// # }
-    /// ```
-    (AccessControlExposeHeaders, ACCESS_CONTROL_EXPOSE_HEADERS) => (Ascii<String>)*
+impl AccessControlExposeHeaders {
+    /// Create an `AccessControlExposeHeaders` from an iterator of header names.
+    pub fn new<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item=::HeaderName>,
+    {
+        let headers = iter
+            .into_iter()
+            .collect();
 
-    test_access_control_expose_headers {
-        test_header!(test1, vec![b"etag, content-length"]);
+        AccessControlExposeHeaders(headers)
     }
 }
