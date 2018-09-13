a334ac9f85927940434ed5b66e3103e21a0cf386
Sean McArthur <sean@seanmonstar.com>
2018-09-13 15:19:44 -0700

    re-enable Location

--- headers-ext/src/common/location.rs
+++ headers-ext/src/common/location.rs
@@ -1,46 +1,60 @@
-header! {
-    /// `Location` header, defined in
-    /// [RFC7231](http://tools.ietf.org/html/rfc7231#section-7.1.2)
-    ///
-    /// The `Location` header field is used in some responses to refer to a
-    /// specific resource in relation to the response.  The type of
-    /// relationship is defined by the combination of request method and
-    /// status code semantics.
-    ///
-    /// # ABNF
-    ///
-    /// ```text
-    /// Location = URI-reference
-    /// ```
-    ///
-    /// # Example values
-    /// * `/People.html#tim`
-    /// * `http://www.example.net/index.html`
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use headers::{Headers, Location};
-    ///
-    /// let mut headers = Headers::new();
-    /// headers.set(Location::new("/People.html#tim"));
-    /// ```
-    ///
-    /// ```
-    /// use headers::{Headers, Location};
-    ///
-    /// let mut headers = Headers::new();
-    /// headers.set(Location::new("http://www.example.com/index.html"));
-    /// ```
-    // TODO: Use URL
-    (Location, LOCATION) => Cow[str]
+use ::HeaderValue;
 
-    test_location {
-        // Testcase from RFC
-        test_header!(test1, vec![b"/People.html#tim"]);
-        test_header!(test2, vec![b"http://www.example.net/index.html"]);
+/// `Location` header, defined in
+/// [RFC7231](http://tools.ietf.org/html/rfc7231#section-7.1.2)
+///
+/// The `Location` header field is used in some responses to refer to a
+/// specific resource in relation to the response.  The type of
+/// relationship is defined by the combination of request method and
+/// status code semantics.
+///
+/// # ABNF
+///
+/// ```text
+/// Location = URI-reference
+/// ```
+///
+/// # Example values
+/// * `/People.html#tim`
+/// * `http://www.example.net/index.html`
+///
+/// # Examples
+///
+/// ```
+/// use headers::{Headers, Location};
+///
+/// let mut headers = Headers::new();
+/// headers.set(Location::new("/People.html#tim"));
+/// ```
+///
+/// ```
+/// use headers::{Headers, Location};
+///
+/// let mut headers = Headers::new();
+/// headers.set(Location::new("http://www.example.com/index.html"));
+/// ```
+//TODO: make this a `Uri`?
+#[derive(Clone, Debug, PartialEq, Header)]
+pub struct Location(HeaderValue);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::super::test_decode;
+
+    #[test]
+    fn absolute_uri() {
+        let s = "http://www.example.net/index.html";
+        let loc = test_decode::<Location>(&[s]).unwrap();
+
+        assert_eq!(loc, Location(HeaderValue::from_static(s)));
     }
 
-}
+    #[test]
+    fn relative_uri_with_fragment() {
+        let s = "/People.html#tim";
+        let loc = test_decode::<Location>(&[s]).unwrap();
 
-bench_header!(bench, Location, { vec![b"http://foo.com/hello:3000".to_vec()] });
+        assert_eq!(loc, Location(HeaderValue::from_static(s)));
+    }
+}
