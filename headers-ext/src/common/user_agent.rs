5a14404e723ff1dcf5c31867b2eadb0b371a77b8
Bastien Orivel <eijebong@bananium.fr>
2018-10-07 01:39:57 +0200

    Add a way to build an UserAgent from something else that a &'static str
    Need to build it from a String in servo.

--- headers-ext/src/common/user_agent.rs
+++ headers-ext/src/common/user_agent.rs
@@ -49,4 +49,10 @@ impl UserAgent {
     pub fn as_str(&self) -> &str {
         self.0.as_str()
     }
+
+    /// Create a `UserAgent` from a string.
+    pub fn from_str(src: &str) -> Option<UserAgent> {
+        let value = HeaderValueString::from_string(src.into())?;
+        Some(UserAgent(value))
+    }
 }
