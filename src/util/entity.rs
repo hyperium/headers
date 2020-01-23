1861bf4c8b55b036571dd3bb6907b349437c31c3
Sean McArthur <sean@seanmonstar.com>
2020-01-23 13:11:14 -0800

    Add precondition_passes methods to IfMatch and IfNoneMatch

--- src/util/entity.rs
+++ src/util/entity.rs
@@ -1,4 +1,4 @@
-use super::IterExt;
+use super::{FlatCsv, IterExt};
 use HeaderValue;
 
 /// An entity tag, defined in [RFC7232](https://tools.ietf.org/html/rfc7232#section-2.3)
@@ -32,7 +32,96 @@ use HeaderValue;
 /// | `W/"1"` | `"1"`   | no match          | match           |
 /// | `"1"`   | `"1"`   | match             | match           |
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub(crate) struct EntityTag(HeaderValue);
+pub(crate) struct EntityTag<T = HeaderValue>(T);
+
+#[derive(Clone, Debug, PartialEq)]
+pub(crate) enum EntityTagRange {
+    Any,
+    Tags(FlatCsv),
+}
+
+// ===== impl EntityTag =====
+
+impl<T: AsRef<[u8]>> EntityTag<T> {
+    /// Get the tag.
+    pub(crate) fn tag(&self) -> &[u8] {
+        let bytes = self.0.as_ref();
+        let end = bytes.len() - 1;
+        if bytes[0] == b'W' {
+            // W/"<tag>"
+            &bytes[3..end]
+        } else {
+            // "<tag>"
+            &bytes[1..end]
+        }
+    }
+
+    /// Return if this is a "weak" tag.
+    pub(crate) fn is_weak(&self) -> bool {
+        self.0.as_ref()[0] == b'W'
+    }
+
+    /// For strong comparison two entity-tags are equivalent if both are not weak and their
+    /// opaque-tags match character-by-character.
+    pub(crate) fn strong_eq<R>(&self, other: &EntityTag<R>) -> bool
+    where
+        R: AsRef<[u8]>,
+    {
+        !self.is_weak() && !other.is_weak() && self.tag() == other.tag()
+    }
+
+    /// For weak comparison two entity-tags are equivalent if their
+    /// opaque-tags match character-by-character, regardless of either or
+    /// both being tagged as "weak".
+    pub(crate) fn weak_eq<R>(&self, other: &EntityTag<R>) -> bool
+    where
+        R: AsRef<[u8]>,
+    {
+        self.tag() == other.tag()
+    }
+
+    /// The inverse of `EntityTag.strong_eq()`.
+    #[cfg(test)]
+    pub(crate) fn strong_ne(&self, other: &EntityTag) -> bool {
+        !self.strong_eq(other)
+    }
+
+    /// The inverse of `EntityTag.weak_eq()`.
+    #[cfg(test)]
+    pub(crate) fn weak_ne(&self, other: &EntityTag) -> bool {
+        !self.weak_eq(other)
+    }
+
+    pub(crate) fn parse(src: T) -> Option<Self> {
+        let slice = src.as_ref();
+        let length = slice.len();
+
+        // Early exits if it doesn't terminate in a DQUOTE.
+        if length < 2 || slice[length - 1] != b'"' {
+            return None;
+        }
+
+        let start = match slice[0] {
+            // "<tag>"
+            b'"' => 1,
+            // W/"<tag>"
+            b'W' => {
+                if length >= 4 && slice[1] == b'/' && slice[2] == b'"' {
+                    3
+                } else {
+                    return None;
+                }
+            }
+            _ => return None,
+        };
+
+        if check_slice_validity(&slice[start..length - 1]) {
+            Some(EntityTag(src))
+        } else {
+            None
+        }
+    }
+}
 
 impl EntityTag {
     /*
@@ -59,6 +148,7 @@ impl EntityTag {
     }
     */
 
+    #[cfg(test)]
     pub fn from_static(bytes: &'static str) -> EntityTag {
         let val = HeaderValue::from_static(bytes);
         match EntityTag::from_val(&val) {
@@ -69,75 +159,11 @@ impl EntityTag {
         }
     }
 
-    /// Get the tag.
-    pub fn tag(&self) -> &[u8] {
-        let bytes = self.0.as_bytes();
-        let end = bytes.len() - 1;
-        if bytes[0] == b'W' {
-            // W/"<tag>"
-            &bytes[3..end]
-        } else {
-            // "<tag>"
-            &bytes[1..end]
-        }
-    }
-
-    /// Return if this is a "weak" tag.
-    pub fn is_weak(&self) -> bool {
-        self.0.as_bytes()[0] == b'W'
-    }
-
-    /// For strong comparison two entity-tags are equivalent if both are not weak and their
-    /// opaque-tags match character-by-character.
-    pub fn strong_eq(&self, other: &EntityTag) -> bool {
-        !self.is_weak() && !other.is_weak() && self.tag() == other.tag()
-    }
-
-    /// For weak comparison two entity-tags are equivalent if their
-    /// opaque-tags match character-by-character, regardless of either or
-    /// both being tagged as "weak".
-    pub fn weak_eq(&self, other: &EntityTag) -> bool {
-        self.tag() == other.tag()
-    }
-
-    /// The inverse of `EntityTag.strong_eq()`.
-    pub fn strong_ne(&self, other: &EntityTag) -> bool {
-        !self.strong_eq(other)
-    }
-
-    /// The inverse of `EntityTag.weak_eq()`.
-    pub fn weak_ne(&self, other: &EntityTag) -> bool {
-        !self.weak_eq(other)
-    }
 
     pub(crate) fn from_val(val: &HeaderValue) -> Option<EntityTag> {
-        let slice = val.as_bytes();
-        let length = slice.len();
-
-        // Early exits if it doesn't terminate in a DQUOTE.
-        if length < 2 || slice[length - 1] != b'"' {
-            return None;
-        }
-
-        let start = match slice[0] {
-            // "<tag>"
-            b'"' => 1,
-            // W/"<tag>"
-            b'W' => {
-                if length >= 4 && slice[1] == b'/' && slice[2] == b'"' {
-                    3
-                } else {
-                    return None;
-                }
-            }
-            _ => return None,
-        };
-
-        if check_slice_validity(&slice[start..length - 1]) {
-            Some(EntityTag(val.clone()))
-        } else {
-            None
-        }
+        EntityTag::parse(val.as_bytes()).map(|_entity| {
+            EntityTag(val.clone())
+        })
     }
 }
 
@@ -182,6 +208,55 @@ fn check_slice_validity(slice: &[u8]) -> bool {
         );
         c != b'"'
     })
+}
+
+// ===== impl EntityTagRange =====
+
+impl EntityTagRange {
+    pub(crate) fn matches_strong(&self, entity: &EntityTag) -> bool {
+        self.matches_if(entity, |a, b| a.strong_eq(b))
+    }
+
+    pub(crate) fn matches_weak(&self, entity: &EntityTag) -> bool {
+        self.matches_if(entity, |a, b| a.weak_eq(b))
+    }
+
+    fn matches_if<F>(&self, entity: &EntityTag, func: F) -> bool
+    where
+        F: Fn(&EntityTag<&str>, &EntityTag) -> bool,
+    {
+        match *self {
+            EntityTagRange::Any => true,
+            EntityTagRange::Tags(ref tags) => {
+                tags.iter()
+                    .flat_map(EntityTag::<&str>::parse)
+                    .any(|tag| func(&tag, entity))
+            },
+        }
+    }
+}
+
+impl super::TryFromValues for EntityTagRange {
+    fn try_from_values<'i, I>(values: &mut I) -> Result<Self, ::Error>
+    where
+        I: Iterator<Item = &'i HeaderValue>,
+    {
+        let flat = FlatCsv::try_from_values(values)?;
+        if flat.value == "*" {
+            Ok(EntityTagRange::Any)
+        } else {
+            Ok(EntityTagRange::Tags(flat))
+        }
+    }
+}
+
+impl<'a> From<&'a EntityTagRange> for HeaderValue {
+    fn from(tag: &'a EntityTagRange) -> HeaderValue {
+        match *tag {
+            EntityTagRange::Any => HeaderValue::from_static("*"),
+            EntityTagRange::Tags(ref tags) => tags.into(),
+        }
+    }
 }
 
 #[cfg(test)]
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,7 +2,7 @@ use HeaderValue;
 
 //pub use self::charset::Charset;
 //pub use self::encoding::Encoding;
-pub(crate) use self::entity::EntityTag;
+pub(crate) use self::entity::{EntityTag, EntityTagRange};
 pub(crate) use self::flat_csv::{FlatCsv, SemiColon};
 pub(crate) use self::fmt::fmt;
 pub(crate) use self::http_date::HttpDate;
