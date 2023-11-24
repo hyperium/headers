ad331d0bd57700e4daadf5c370ff9e2295dd8b8a
Sean McArthur <sean@seanmonstar.com>
2023-11-24 14:15:00 -0500

    headers:v0.4.0

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 #![deny(missing_debug_implementations)]
 #![cfg_attr(test, deny(warnings))]
 #![cfg_attr(all(test, feature = "nightly"), feature(test))]
-#![doc(html_root_url = "https://docs.rs/headers/0.3.9")]
+#![doc(html_root_url = "https://docs.rs/headers/0.4.0")]
 
 //! # Typed HTTP Headers
 //!
