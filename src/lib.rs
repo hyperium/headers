e2a2ea244a590a3a092e8ef44f62895fd696ced1
tison <wander4096@gmail.com>
2025-06-02 15:09:57 +0800

    crates.io has been linking to docs.rs by default
    Signed-off-by: tison <wander4096@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@
 #![deny(missing_debug_implementations)]
 #![cfg_attr(test, deny(warnings))]
 #![cfg_attr(all(test, feature = "nightly"), feature(test))]
-#![doc(html_root_url = "https://docs.rs/headers/0.4.1")]
 
 //! # Typed HTTP Headers
 //!
